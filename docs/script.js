1ee5553641ea49ecbb19a8b4a7272ba88c3922f5
Александр <sacha3032104@gmail.com>
2021-04-06 21:32:56 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
 for (const $downloadButton of document.querySelectorAll('.download')) {
     const version = $downloadButton.getAttribute('version')
-    $downloadButton.href = `https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop/releases/download/${version}/win_x86.rar`
+    $downloadButton.href = `https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop/releases/download/${version}/SnowRunnerXMLEditor_x86.rar`
 }
