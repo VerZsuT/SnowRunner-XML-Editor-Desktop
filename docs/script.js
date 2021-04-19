c7d29c022eee3ccdda8b6303a3af899d360075df
Александр <sacha3032104@gmail.com>
2021-04-20 01:04:23 +0300

    Исправление

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,8 @@
 for (const $downloadButton of document.querySelectorAll('.download')) {
+    const a = document.createElement('a')
     const version = $downloadButton.getAttribute('version')
-    $downloadButton.href = `https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop/releases/download/${version}/SnowRunnerXMLEditor_x86.rar`
+    a.style.display = 'none'
+    $downloadButton.prepend(a)
+    a.href = `https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop/releases/download/${version}/SnowRunnerXMLEditor_x86.rar`
+    $downloadButton.onclick = () => a.click()
 }
