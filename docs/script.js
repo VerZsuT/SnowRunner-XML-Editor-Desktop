2155bd0cc2a34a6dda0c0d8189c79b870d4beb9c
Александр <sacha3032104@gmail.com>
2021-09-22 19:33:41 +0300

    Добавил самораспаковывающийся архив.

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,9 @@
 for (const $downloadButton of document.querySelectorAll('.download')) {
-    const a = document.createElement('a')
-    const version = $downloadButton.getAttribute('version')
-    a.style.display = 'none'
-    $downloadButton.prepend(a)
-    a.href = `https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop/releases/download/${version}/SnowRunnerXMLEditor.rar`
-    $downloadButton.onclick = () => a.click()
+    const a = document.createElement('a');
+    const version = $downloadButton.getAttribute('version');
+    const type = $downloadButton.getAttribute('type');
+    a.style.display = 'none';
+    $downloadButton.prepend(a);
+    a.href = `https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop/releases/download/${version}/SnowRunnerXMLEditor.${(type==='exe')?'exe':'rar'}`;
+    $downloadButton.onclick = () => a.click();
 }
