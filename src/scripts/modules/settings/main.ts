7ee93f3cf2e2032a66de2403a541de4042d85cba
Александр <sacha3032104@gmail.com>
2021-12-03 14:16:35 +0300

    Изменено расширение с .ts на .tsx

--- src/scripts/modules/settings/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { t, mainProcess } from '../../service'
-import App from './components/App.vue'
-
-settingsPreload.errorHandler = message => mainProcess.alertSync(t[`${message}`.replace('Error: ', '')])
-
-createApp(App).mount('#main')
-
