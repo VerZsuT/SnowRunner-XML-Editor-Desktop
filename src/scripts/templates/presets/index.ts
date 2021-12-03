50f59ffe5017ea0583fe341a92a4a8acf731bd7d
Александр <sacha3032104@gmail.com>
2021-12-03 14:35:34 +0300

    Добавлен реэкспорт

---
+++ src/scripts/templates/presets/index.ts
@@ -0,0 +1,2 @@
+export { default as unlockGroup } from './unlockGroup'
+export { default as gear } from './gear'
---
+++ src/scripts/texts/index.ts
@@ -0,0 +1,3 @@
+export { default as RU } from './RU.json'
+export { default as EN } from './EN.json'
+export { default as DE } from './DE.json'
