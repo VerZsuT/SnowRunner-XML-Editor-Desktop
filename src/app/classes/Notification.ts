e52afd333c5e14f9c44a25de717899b97ab2bdd9
Александр <sacha3032104@gmail.com>
2021-12-03 14:09:05 +0300

    Переход с Promise на async.

--- src/app/classes/Notification.ts
+++ src/app/classes/Notification.ts
@@ -9,18 +9,16 @@ type TKeys = keyof typeof RU
 /** Отвечает за показ `windows-notification`. */
 export default class Notification {
     /** Выводит windows-notification на экран. */
-    public static show = (titleKey: TKeys, messageKey: TKeys) => {
-        return new Promise(resolve => {
-            if (ElNotification.isSupported()) {
-                const notification = new ElNotification({
-                    title: Texts.get(titleKey),
-                    icon: paths.icon,
-                    body: Texts.get(messageKey)
-                })
-    
-                notification.show()
-                notification.once('click', resolve)
-            }
-        })
+    public static show = async (titleKey: TKeys, messageKey: TKeys) => {
+        if (ElNotification.isSupported()) {
+            const notification = new ElNotification({
+                title: Texts.get(titleKey),
+                icon: paths.icon,
+                body: Texts.get(messageKey)
+            })
+
+            notification.show()
+            await new Promise(resolve => notification.once('click', resolve))
+        }
     }
 }
