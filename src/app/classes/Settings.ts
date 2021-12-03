fe5eb4c7fa063576dfb927d0071098dccb737ba6
Александр <sacha3032104@gmail.com>
2021-12-03 14:10:59 +0300

    Добавлен метод set для инициализации в index файле

--- src/app/classes/Settings.ts
+++ src/app/classes/Settings.ts
@@ -1,4 +1,10 @@
 /** Для неявной передачи настроек в остальные классы программы. */
 export default class Settings {
     public static obj: ISettings = {}
+    public static set(newObj: ISettings): ISettings {    
+        for (const key in newObj) {
+            this.obj[key] = newObj[key]
+        }
+        return this.obj
+    }
 }
