80a8219453bb3cc72d8c96e44713728187304153
Александр <sacha3032104@gmail.com>
2021-12-03 14:08:42 +0300

    Незначительные изменения.

--- src/app/classes/Menu.ts
+++ src/app/classes/Menu.ts
@@ -8,98 +8,96 @@ export default class Menu {
     private static config: IConfig = Config.obj
 
     /** Возвращает шаблон меню. */
-    public static get = (): IMenuTemplate[] => {
-        return [
-            {
-                label: Texts.get('FILE_MENU_LABEL'),
-                submenu: [
-                    ...this.ifHasInitial([
-                        {
-                            label: Texts.get('SETTINGS_MENU_ITEM_LABEL'),
-                            role: MenuRole.openSettings
-                        },
-                        { role: MenuRole.separator }
-                    ]),
-                    ...this.ifDevBuild(this.ifHasInitial([
-                        {
-                            label: Texts.get('RESET_MENU_ITEM_LABEL'),
-                            role: MenuRole.resetConfig
-                        }
-                    ])),
-                    ...this.ifDevBuild([
-                        {
-                            label: 'DevTools',
-                            role: MenuRole.devTools
-                        },
-                        {
-                            label: 'Reload',
-                            role: MenuRole.reload
-                        }
-                    ]),
+    public static get = (): IMenuTemplate[] => [
+        {
+            label: Texts.get('FILE_MENU_LABEL'),
+            submenu: [
+                ...this.ifHasInitial([
                     {
-                        label: Texts.get('EXIT_MENU_ITEM_LABEL'),
-                        role: MenuRole.quitApp
+                        label: Texts.get('SETTINGS_MENU_ITEM_LABEL'),
+                        role: MenuRole.openSettings
+                    },
+                    { role: MenuRole.separator }
+                ]),
+                ...this.ifDevBuild(this.ifHasInitial([
+                    {
+                        label: Texts.get('RESET_MENU_ITEM_LABEL'),
+                        role: MenuRole.resetConfig
                     }
-                ]
-            },
-            ...this.ifHasInitial([
+                ])),
+                ...this.ifDevBuild([
+                    {
+                        label: 'DevTools',
+                        role: MenuRole.devTools
+                    },
+                    {
+                        label: 'Reload',
+                        role: MenuRole.reload
+                    }
+                ]),
                 {
-                    label: Texts.get('BACKUP_MENU_LABEL'),
-                    submenu: [
-                        {
-                            label: Texts.get('OPEN_BUTTON'),
-                            role: MenuRole.showFolder,
-                            path: paths.backupFolder
-                        },
-                        { role: MenuRole.separator },
-                        {
-                            label: Texts.get('SAVE_BUTTON'),
-                            role: MenuRole.saveBackup
-                        },
-                        {
-                            label: Texts.get('RESTORE_MENU_ITEM_LABEL'),
-                            role: MenuRole.recoverFromBackup
-                        }
-                    ]
+                    label: Texts.get('EXIT_MENU_ITEM_LABEL'),
+                    role: MenuRole.quitApp
                 }
-            ]),
-            ...this.ifHasInitial([
-                {
-                    label: Texts.get('EPF_MENU_LABEL'),
-                    submenu: [
-                        {
-                            label: Texts.get('JOIN_EXPORTED_FILES'),
-                            role: MenuRole.joinEPF
-                        },
-                        {
-                            label: Texts.get('SEE_EXPORTED_FILE'),
-                            role: MenuRole.seeEPF
-                        }
-                    ]
-                }
-            ]),
+            ]
+        },
+        ...this.ifHasInitial([
             {
-                label: Texts.get('HELP_MENU_LABEL'),
+                label: Texts.get('BACKUP_MENU_LABEL'),
                 submenu: [
                     {
-                        label: Texts.get('HOW_TO_USE_TITLE'),
-                        role: MenuRole.openURL,
-                        url: 'https://snowrunner.mod.io/guides/snowrunner-xml-editor'
+                        label: Texts.get('OPEN_BUTTON'),
+                        role: MenuRole.showFolder,
+                        path: paths.backupFolder
+                    },
+                    { role: MenuRole.separator },
+                    {
+                        label: Texts.get('SAVE_BUTTON'),
+                        role: MenuRole.saveBackup
                     },
                     {
-                        label: 'GitHub',
-                        role: MenuRole.openURL,
-                        url: 'https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop'
-                    },
-                    {
-                        label: 'YouTube(RU)',
-                        role: MenuRole.openURL,
-                        url: 'https://youtube.com/playlist?list=PLDwd4yUwzS2VtWCpC9X6MXm47Kv_s_mq2'
+                        label: Texts.get('RESTORE_MENU_ITEM_LABEL'),
+                        role: MenuRole.recoverFromBackup
                     }
                 ]
             }
-        ]
-    }
+        ]),
+        ...this.ifHasInitial([
+            {
+                label: Texts.get('EPF_MENU_LABEL'),
+                submenu: [
+                    {
+                        label: Texts.get('JOIN_EXPORTED_FILES'),
+                        role: MenuRole.joinEPF
+                    },
+                    {
+                        label: Texts.get('SEE_EXPORTED_FILE'),
+                        role: MenuRole.seeEPF
+                    }
+                ]
+            }
+        ]),
+        {
+            label: Texts.get('HELP_MENU_LABEL'),
+            submenu: [
+                {
+                    label: Texts.get('HOW_TO_USE_TITLE'),
+                    role: MenuRole.openURL,
+                    url: 'https://snowrunner.mod.io/guides/snowrunner-xml-editor'
+                },
+                {
+                    label: 'GitHub',
+                    role: MenuRole.openURL,
+                    url: 'https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop'
+                },
+                {
+                    label: 'YouTube(RU)',
+                    role: MenuRole.openURL,
+                    url: 'https://youtube.com/playlist?list=PLDwd4yUwzS2VtWCpC9X6MXm47Kv_s_mq2'
+                }
+            ]
+        }
+    ]
 
     /** Возвращает переданный шаблон если присутствует путь к `initial.pak`. */
     private static ifHasInitial = (menu: IMenuTemplate[]): IMenuTemplate[] => {
