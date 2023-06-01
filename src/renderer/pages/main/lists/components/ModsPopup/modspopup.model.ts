f1b3b5ce1b8f356c57694101f37381017d39941d
Александр <verzsut@gmail.com>
2023-06-01 21:35:04 +0300

    Fix texts

--- src/renderer/pages/main/lists/components/ModsPopup/modspopup.model.ts
+++ src/renderer/pages/main/lists/components/ModsPopup/modspopup.model.ts
@@ -2,9 +2,10 @@ import type IModsPopupProps from './modspopup.props'
 
 import type { IFindItem } from '#g/types'
 import { prop, reactive, ViewModel } from '#r/model-ctrlr'
+import $ from '../../texts'
 
 class ModsPopupModel extends ViewModel<IModsPopupProps> {
-  readonly titles = ['Found', 'Added']
+  readonly titles = [$.FOUND_ITEMS, $.ADDED_ITEMS]
 
   @prop<IModsPopupProps>('show')
   readonly show!: IModsPopupProps['show']
