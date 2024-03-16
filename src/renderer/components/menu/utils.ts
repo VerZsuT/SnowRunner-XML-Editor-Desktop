6d3b0e7d3b66a656759a41702e76578d8e1bccb2
VerZsuT <sacha3032104@gmail.com>
2024-03-16 10:46:17 +0300

    add donation menu item

--- src/renderer/components/menu/utils.ts
+++ src/renderer/components/menu/utils.ts
@@ -5,5 +5,7 @@ export const links = {
   /** Ссылка на `github.com` */
   github: 'https://github.com/VerZsuT/SnowRunner-XML-Editor-Desktop',
   /** Ссылка на `youtube.com` */
-  youtube: 'https://youtube.com/playlist?list=PLDwd4yUwzS2VtWCpC9X6MXm47Kv_s_mq2'
+  youtube: 'https://youtube.com/playlist?list=PLDwd4yUwzS2VtWCpC9X6MXm47Kv_s_mq2',
+  /** Ссылка на `donationalerts.com` */
+  donation: 'https://www.donationalerts.com/r/verzsut'
 }
